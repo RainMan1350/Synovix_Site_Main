3e06ce126c3ed8f33cca1b9b4e91ebcbc380e158
RainMan1350 <justin.a.hall38@gmail.com>
2025-03-05 09:36:18 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // us-east-1_MwOGaPF6j
-        userPoolClientId: '', // 3crvfl08acb8uqb23vs2djl3q8
-        region: '' // us-east-1
+        userPoolId: 'us-east-1_MwOGaPF6j', // us-east-1_MwOGaPF6j
+        userPoolClientId: '3crvfl08acb8uqb23vs2djl3q8', // 3crvfl08acb8uqb23vs2djl3q8
+        region: 'us-east-1' // us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
