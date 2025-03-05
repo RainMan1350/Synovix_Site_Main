83eab32f59e55e5b92b17fbaa672eb7ea460067a
RainMan1350 <justin.a.hall38@gmail.com>
2025-03-05 09:55:04 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_MwOGaPF6j', // us-east-1_MwOGaPF6j
-        userPoolClientId: '3crvfl08acb8uqb23vs2djl3q8', // 3crvfl08acb8uqb23vs2djl3q8
+        userPoolId: 'us-east-1_ORcYwyesd', // us-east-1_MwOGaPF6j
+        userPoolClientId: '3jis9nflmbtbbmgp8eu4k489i0', // 3crvfl08acb8uqb23vs2djl3q8
         region: 'us-east-1' // us-east-1
     },
     api: {
